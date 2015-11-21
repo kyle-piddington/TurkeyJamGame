9b37dbb9b24a8f190661f46ce6cc191a27ede2ad
willle3456 <willle3456@gmail.com>
2015-11-20 20:12:44 -0800

    test map and map code, that currently doesn't work

--- core/src/com/mygdx/game/TurkeyJam.java
+++ core/src/com/mygdx/game/TurkeyJam.java
@@ -2,26 +2,116 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
-public class TurkeyJam extends ApplicationAdapter {
-	SpriteBatch batch;
+public class TurkeyJam extends ApplicationAdapter implements InputProcessor{
+	//SpriteBatch batch;
 	Texture img;
+	TiledMap tiledMap;
+	OrthographicCamera camera;
+	TiledMapRenderer tiledMapRenderer;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//batch = new SpriteBatch();
+		//img = new Texture("badlogic.jpg");
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false,w,h);
+		camera.update();
+		tiledMap = new TmxMapLoader().load("rpg_test_map.tmx");
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		camera.update();
+		tiledMapRenderer.setView(camera);
+		tiledMapRenderer.render();
+		//batch.begin();
+		//batch.draw(img, 0, 0);
+		//batch.end();
+	}
+
+	@Override
+	public boolean keyDown(int keycode)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode)
+	{
+		switch (keycode)
+		{
+			case Input.Keys.LEFT:
+				camera.translate(-32,0);
+				break;
+			case Input.Keys.RIGHT:
+				camera.translate(32,0);
+				break;
+			case Input.Keys.UP:
+				camera.translate(0,-32);
+				break;
+			case Input.Keys.DOWN:
+				camera.translate(0,32);
+				break;
+			case Input.Keys.NUM_1:
+				tiledMap.getLayers().get(0).setVisible(!tiledMap.getLayers().get(0).isVisible());
+				break;
+			case Input.Keys.NUM_2:
+				tiledMap.getLayers().get(1).setVisible(!tiledMap.getLayers().get(1).isVisible());
+				break;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button)
+	{
+		return false;
+	}
+
+	public boolean touchUp(int screenX, int screenY, int pointer, int button)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int point)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount)
+	{
+		return false;
 	}
 }
